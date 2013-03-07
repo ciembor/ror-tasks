a727894a1e729aeb15217b17a3e4ef70fc263aff
ciembor <pub@ciemborowicz.pl>
2013-03-07 21:02:04 +0100

    resolved tests

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,37 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+  	raise IllegalArgument if items.nil?
+  	@items = items
+  	@completed = []
   end
+
+  def empty?
+  	@items.empty?
+  end
+
+  def size
+  	@items.size
+  end
+
+  def <<(item)
+  	@items.push(item)
+  end
+
+  def first
+  	@items.last
+  end
+
+  def last
+  	@items.last
+  end
+
+  def completed?(index)
+  	true if @completed.include?(index)
+  end
+
+  def complete(index)
+  	@completed.push(index)
+  end
+
 end
