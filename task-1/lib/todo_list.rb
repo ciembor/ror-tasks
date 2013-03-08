a489e88d48f1348d0ec1cfc858d1b8349c1278c9
ciembor <pub@ciemborowicz.pl>
2013-03-08 22:59:31 +0100

    First exercises.

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -1,10 +1,35 @@
 class TodoList
 
+  class Item
+  	
+    def initialize(description=nil)
+  	  @description = description
+  	  @completed = false
+  	end
+
+    def to_s
+      @description.to_s
+    end
+
+    def formated
+      if @completed
+        completion = "x"
+      else
+        completion = " "
+      end
+      "- [#{completion}] #{@description}"
+    end
+
+  	attr_accessor :description, :completed
+  end
+
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
   	raise IllegalArgument if items.nil?
-  	@items = items
-  	@completed = []
+  	@items = []
+    items.each do |item|
+      @items.push Item.new(item)
+    end
   end
 
   def empty?
@@ -16,23 +41,89 @@ class TodoList
   end
 
   def <<(item)
-  	@items.push(item)
+  	@items.push(Item.new(item))
   end
 
   def first
-  	@items.last
+  	@items.first
   end
 
   def last
-  	@items.last
+  	@items.first
   end
 
   def completed?(index)
-  	true if @completed.include?(index)
+    @items[index].completed
   end
 
   def complete(index)
-  	@completed.push(index)
+  	@items[index].completed = true
+  end
+
+  def completed
+    @items.select { |item| item.completed }
+  end
+
+  def uncompleted
+    @items.reject { |item| item.completed }
+  end
+
+  def remove(index)
+    if (index >= 0 and index < @items.size)
+      @items.delete_at(index)
+    else
+      raise IndexOutOfBounds
+    end
+  end
+
+  def remove_completed
+    @items.reject! { |item| item.completed }
+  end
+
+  def revert(*args)
+    case args.size
+    when 0
+      @items.reverse!
+    when 2
+      [args[0], args[1]].each do |index|
+        raise IndexOutOfBounds if (index < 0 or index >= @items.size)
+      end
+      tmp = @items[args[0]]
+      @items[args[0]] = @items[args[1]]
+      @items[args[1]] = tmp
+    end
+  end
+
+  def item(index)
+    @items[index]
+  end
+
+  def toggle(index)
+    if (index >= 0 and index < @items.size)
+      if self.completed?(index)
+        self.uncomplete(index)
+      else
+        self.complete(index)
+      end
+    else
+      raise IndexOutOfBounds
+    end
+  end
+
+  def uncomplete(index)
+    if (index >= 0 and index < @items.size)
+      @items[index].completed = false
+    else
+      raise IndexOutOfBounds
+    end
+  end
+
+  def sort
+    @items.sort! { |a, b| a.description.downcase <=> b.description.downcase }
+  end
+
+  def to_s
+    (@items.collect { |item| item.formated }).join("\n")
   end
 
 end
--- task-1/spec/todo_list.rb
+++ task-1/spec/todo_list.rb
@@ -7,7 +7,8 @@ describe TodoList do
   subject(:list)            { TodoList.new(items) }
   let(:items)               { [] }
   let(:item_description)    { "Buy toilet paper" }
-
+  let(:another_item_description)  { "Read book" }
+  let(:yet_another_item_description)  { "Alabama" }
   it { should be_empty }
 
   it "should raise an exception when nil is passed to the constructor" do
@@ -55,4 +56,94 @@ describe TodoList do
       list.completed?(0).should be_true
     end
   end
+
+  context "with many items" do
+    let(:items)             { [item_description, another_item_description, yet_another_item_description] }
+
+    it "should return completed items" do
+      for i in (0...list.size) do
+        list.completed.size.should == i
+        list.complete(i)
+      end
+    end
+
+    it "should return uncompleted items" do
+      for i in (0...list.size) do
+        list.uncompleted.size.should == (list.size - i)
+        list.complete(i)
+      end
+    end
+
+    it "should remove individual items" do
+      expect {list.remove(list.size)}.to raise_error(IndexOutOfBounds)
+      removed = list.first.to_s
+      list.remove(0)
+      list.first.to_s.should_not == removed
+    end
+
+    it "should remove all completed items" do
+      size_before = list.size
+      list.complete(0)
+      list.remove_completed
+      list.size.should == size_before - 1
+    end
+
+    it "should revert order of two items" do
+      first = list.item(0)
+      second = list.item(1)
+      list.revert(0, 1)
+      second.should == list.item(0)
+      first.should == list.item(1)
+      expect {list.revert(0, list.size)}.to raise_error(IndexOutOfBounds)
+      expect {list.revert(list.size, 0)}.to raise_error(IndexOutOfBounds)
+    end
+
+    it "should revert order of all items" do
+      items = []
+      for i in (0...list.size) do
+        items.push(list.item(i))
+      end
+      list.revert
+      for i in (0...list.size) do
+        items[items.size - i - 1].to_s.should == list.item(i).to_s
+      end
+    end
+
+    it "should toggle state of an item" do
+      expect {list.toggle(list.size)}.to raise_error(IndexOutOfBounds)
+      2.times do
+        list.toggle(0)
+        list.completed?(0).should be_true
+        list.toggle(0)
+        list.completed?(0).should be_false
+      end
+    end
+
+    it "should set state of item to uncompleted" do
+      list.complete(0)
+      expect {list.uncomplete(list.size)}.to raise_error(IndexOutOfBounds)
+      list.uncomplete(0)
+      list.completed?(0).should be_false
+    end
+
+    it "should change the description of an item" do
+      list.first.to_s.should_not == another_item_description
+      list.first.description = another_item_description
+      list.first.to_s.should == another_item_description
+    end
+
+    it "should sort items by name" do
+      list.sort
+      for i in (0...(list.size - 1)) do
+        list.item(i).to_s.should <= list.item(i + 1).to_s
+      end
+    end
+
+    it "should convert the list to text with the following format:\n- [ ] Uncompleted item\n- [x] Completed item" do
+      list.complete(1)
+      list.to_s.should == "- [ ] #{item_description}\n- [x] #{another_item_description}\n- [ ] #{yet_another_item_description}"
+    end
+
+  end
+
 end
